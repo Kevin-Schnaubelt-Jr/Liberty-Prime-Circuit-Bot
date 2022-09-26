a58498086dba9d6569b4facb9f8929bf7aa72ba4
Media Lab <MedLab@tcl.edu>
2022-09-26 09:09:09 -0400

    class notes

---
+++ AC_class.py
@@ -0,0 +1,37 @@
+''' AC changes in both magnitude and direction.
+Z = Impedance : Measure of the opposition that a circuit presents to a current when
+a voltage is applied.
+
+AC
+==
+V = I*Z
+I = V/Z
+Z = V/I
+
+Resistors will be represented as real numbers, while capacitors and inductors will be represented as imaginary.
+
+T = Period : Time required to produce one complete cycle of a waveform.
+
+Cycle is one complete positive & negative alternation of the waveform. 
+
+F = Frequency : The number of cycles per second. Measured in hertz (Hz).
+
+F = 1 / T
+
+T = 1 / F
+
+120Vrms @ 60Hz. 
+Period = 1/60 
+
+Vp = 0 to max positive peak on sin wave.
+
+Vrms = .707 * Vp
+
+
+'''
+
+f = 60 #60Hz
+
+t = 1/f
+
+print(t)
