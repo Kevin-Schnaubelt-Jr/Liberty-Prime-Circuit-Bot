37c1356718270210972de8f486e428a702321ef3
Media Lab <MedLab@tcl.edu>
2022-10-05 09:22:57 -0400

    more nodes

--- AC_class.py
+++ AC_class.py
@@ -69,6 +69,8 @@ print(t)
 
 '''
 
+
+#rectangle to polar?
 '''
 
 z = 3+4j
@@ -77,14 +79,14 @@ c = math.sqrt((z.real * 2) + (z.imag * 2)) # rectangular form, <int>
 
 q = math.degrees(math.atan(z.imag/z.real)) # polar form. Notice the order, in degrees
 
-print(q)
-
-
-'''
-
+print(c, q)
 
 '''
+
+
+
 # polar to rectangular convert
+'''
 # Z = 4 and L45degrees
 
 z = complex((4 * math.cos(math.radians(45))), (4 * math.sin(math.radians(45))))
@@ -97,3 +99,63 @@ Xl = 2 pi f L
 xc = 1/(2 pi f c)
 
 '''
+
+'''
+Vs = 120 # Vrms
+F = 60 #Hz
+R = 100 #ohms
+C = 1 #mF .000001 F
+C = 0.000001
+L = 1 #mH .001 H
+L = .001
+
+#Reactence of capacitor
+Xc = 1 / (2 * math.pi * F * C) 
+
+print(Xc)
+#Reactence of inductor
+Xl = 2 * math.pi * F * L
+print(Xl)
+#Impedence of capacitor
+# Zc = Xc 
+# print(Zt)
+
+#Impedence of inductor
+
+# Is = Vs/Zt
+
+y = complex(100, Zt)
+ypi = (y.real * 2) + (y.imag * 2)
+ypi = math.sqrt(ypi)
+
+print(ypi)
+'''
+
+#example 2
+
+Vs = 10 #V
+F = 60 #Hz
+R = 100 #ohms
+C = 10 #mF = .000010F
+C = .000010
+L = 20 #mh = .020 H
+L = .020
+
+Xc = 1 / (2 * math.pi * F * C)
+print('Xc',Xc)
+
+Xl = 2 * math.pi * F * L
+print('Xl',Xl)
+
+Zr = 100 #ohms +-0j
+# Zc = ?
+# Zt = 274.44 # -68.793 ?should be?
+
+Zt = complex(-Xc, Xl)
+print(Zt)
+
+Zt = math.sqrt(abs((Zt.real * 2)) + (Zt.imag * 2))
+
+print(Zt)
+
+
